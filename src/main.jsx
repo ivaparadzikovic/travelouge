3534dd16a6282b4d674d5377eed9206637046a99
Iva Paradžikovic <ivaparadzikovic@gmail.com>
2026-03-10 20:39:41 +0100

    add translations

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.jsx'
 import AuthProvider from './context/AuthProvider.jsx'
 import './index.css'
+import './i18n'
 
 const queryClient = new QueryClient({
   defaultOptions: {
