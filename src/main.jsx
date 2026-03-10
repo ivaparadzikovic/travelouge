6c9bc71e10056718852636dde15c1caf7a5631f7
Iva Paradžikovic <ivaparadzikovic@gmail.com>
2026-03-10 20:40:37 +0100

    add toasts

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'react-hot-toast'
 import App from './App.jsx'
 import AuthProvider from './context/AuthProvider.jsx'
 import './index.css'
@@ -20,6 +21,16 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <App />
+        <Toaster
+          position="bottom-right"
+          toastOptions={{
+            duration: 4000,
+            style: {
+              background: '#333',
+              color: '#fff',
+            },
+          }}
+        />
       </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>,
